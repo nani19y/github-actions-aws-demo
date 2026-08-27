31a85c73501092be096aa487f708ecffe1cdead1
jashwanth <nagaralajashwanthsagar@gmail.com>
2026-08-27 15:19:17 +0530

    Test CI failure

--- test_app.py
+++ test_app.py
@@ -4,8 +4,8 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-
-    assert response.status_code == 200
+    assert response.status_code == 500
+   # assert response.status_code == 200
     assert response.data == b"Hello from GitHub Actions + AWS EC2!"
 
 
